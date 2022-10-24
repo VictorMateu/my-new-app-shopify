10512e81f45a0f652b9347fefb0f2d3f39b7b7f4
Victor Mateu <victormateulopez@gmail.com>
2022-10-24 13:34:06 +0200

    algunos cambios

--- web/index.js
+++ web/index.js
@@ -1,5 +1,5 @@
 // @ts-check
-import 'dotenv/config';   // posible error?
+//import 'dotenv/config';   // posible error?
 import { join } from "path";
 import { readFileSync } from "fs";
 import express from "express";
